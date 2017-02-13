ac223a3982ff813a861dffa9217ad51952864cea
Ivan <cherednichenko.ivan1@gmail.com>
2017-02-14 01:17:57 +0200

    hello

---
+++ Lesson1.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.printf("%s %n",new Date());
+        System.out.printf("%h %n",'h');
+        System.out.println((int)'h');
+        double real = Math.PI;
+        System.out.format(new Locale("uk","uk"),"%-20.10f  %n",real);
+        System.out.printf("%tc",new Date(-(1900+100-1),3,1));
+    }
+}
